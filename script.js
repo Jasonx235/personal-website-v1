7d25851cb7496daf1db9cd90cc47be79b8aa78d5
Jason Mui <Jasonmui22@gmail.com>
2021-03-11 15:08:59 -0500

    Add small Js for mobile menu

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Hello");
+
+const mobileMenuButtonHAM = document.getElementById("mobile-menu-button-ham");
+const mobileMenuButtonX = document.getElementById("mobile-menu-button-x");
+const mobileMenu = document.getElementById("mobile-menu");
+
+mobileMenuButtonHAM.addEventListener("click", function () {
+  if (mobileMenu.classList.contains("closed")) {
+    mobileMenuButtonHAM.classList.add("closed");
+    mobileMenu.classList.remove("closed");
+    mobileMenuButtonX.classList.remove("closed");
+  } else {
+    mobileMenuButtonHAM.classList.remove("closed");
+    mobileMenu.classList.add("closed");
+    mobileMenuButtonX.classList.add("closed");
+  }
+});
+
+mobileMenuButtonX.addEventListener("click", function () {
+  if (mobileMenu.classList.contains("closed")) {
+    mobileMenu.classList.remove("closed");
+    mobileMenuButtonHAM.classList.add("closed");
+    mobileMenuButtonX.classList.remove("closed");
+  } else {
+    mobileMenu.classList.add("closed");
+    mobileMenuButtonHAM.classList.remove("closed");
+    mobileMenuButtonX.classList.add("closed");
+  }
+});
